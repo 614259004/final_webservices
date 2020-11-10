3902073ebd48beb9e2df038c49ecf0c1eecfb758
unknown <614259004@webmail.npru.ac.th>
2020-11-10 15:29:45 +0700

    add new project

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const myData = require('../topTenRecovered');
-const Coviddata = require('../data');
+const Coviddata = require('../coviddata');
 
 router.get('/',(req,res)=>{
     res.render('index',{Coviddata});
